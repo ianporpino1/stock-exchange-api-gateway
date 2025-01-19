c42ac33a33c76d9f0c9b8395b9a9b2faa94e49a2
ianporpino1 <ianporpino127@gmail.com>
2025-01-18 21:30:04 -0300

    jwt oauth2 impl ok

--- src/main/java/com/stockexchange/apigateway/security/SecurityConfig.java
+++ src/main/java/com/stockexchange/apigateway/security/SecurityConfig.java
@@ -1,51 +1,28 @@
 package com.stockexchange.apigateway.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.util.FileCopyUtils;
-import java.io.InputStreamReader;
-import java.security.KeyFactory;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
-
-    @Value("${jwt.public.key}")
-    private Resource publicKeyResource;
-
+    
     @Bean
-    public JwtDecoder jwtDecoder() throws Exception {
-        return NimbusJwtDecoder.withPublicKey(rsaPublicKey()).build();
-    }
-    private String readPublicKeyAsString() throws Exception {
-        try (InputStreamReader reader = new InputStreamReader(publicKeyResource.getInputStream())) {
-            return FileCopyUtils.copyToString(reader);
-        }
-    }
-
-    @Bean
-    public RSAPublicKey rsaPublicKey() throws Exception {
-        try {
-            String publicKeyContent = readPublicKeyAsString();
-            String cleanKey = publicKeyContent
-                    .replace("-----BEGIN PUBLIC KEY-----", "")
-                    .replace("-----END PUBLIC KEY-----", "")
-                    .replaceAll("\\s+", "");
-            byte[] encoded = Base64.getDecoder().decode(cleanKey);
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
-
-        } catch (Exception e) {
-            System.err.println("Erro ao ler arquivo de chave pública: " + e.getMessage());
-            e.printStackTrace();
-            throw new Exception("Erro ao processar chave pública do arquivo: " + e.getMessage(), e);
-        }
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/user-service/users").permitAll()
+                        .pathMatchers("/user-service/auth/login").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
     }
 }
